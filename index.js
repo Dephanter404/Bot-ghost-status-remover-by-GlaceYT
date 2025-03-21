a5ad148015cf3c7fc828fd66c93cc4f18a6860bc
Dephanter404 <103958927+Dephanter404@users.noreply.github.com>
2025-03-21 22:55:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["S'occupe de l'Illégal"];
+const statusMessages = ["Prépare un nouveau braquage"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
